d8ad1f395e1fcc3b70d6187ed65383fadc4aefb1
olehkyshlyan <37051231+olehkyshlyan@users.noreply.github.com>
2019-10-26 07:46:00 +0300

    Indexpageelements file | 2019.10.26

--- indexpageelements.php
+++ indexpageelements.php
@@ -1,20 +1,43 @@
 <?
+namespace IndexPageElements
+{
 
 // no reason to use static property or method
 // just for learning purposes
 
-class AskAQuestionButton
+class MiddleBlockMainButtons
 {
-	public static $button = NULL;
+	public static $sectionFirstPart = NULL;
+	public static $sectionSecondPart = NULL;
+	public static $categoriesButton = NULL;
+	public static $askAQuestionButton = NULL;
 	
-	public static function createButton(){
-		self::$button = '<div id="wrMBAskWord">';
-		self::$button .= '<span id="mbAskWord">Ask a question !</span>';
-		self::$button .= '</div>';
+	public static function createWrapperSection(){
+		self::$sectionFirstPart = '<div id="middleBlockMainButtons">';
+		self::$sectionSecondPart = '</div>';
+	}
+	
+	public static function createCategoriesButton(){
+		self::$categoriesButton = '<div id="wrCategoriesButton">';
+		self::$categoriesButton .= '<span id="categoriesButton">Categories</span>';
+		self::$categoriesButton .= '</div>';
+	}
+	
+	public static function createAskAQuestionButton(){
+		self::$askAQuestionButton = '<div id="wrMBAskWord">';
+		self::$askAQuestionButton .= '<span id="mbAskWord">Ask a question !</span>';
+		self::$askAQuestionButton .= '</div>';
+	}
+	
+	public static function constructImitation(){
+		self::createWrapperSection();
+		self::createCategoriesButton();
+		self::createAskAQuestionButton();
 	}
 	
 }
 
-AskAQuestionButton::createButton();
+MiddleBlockMainButtons::constructImitation();
 
+}
 ?>
